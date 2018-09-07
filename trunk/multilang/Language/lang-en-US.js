4f459017d3ca0b0a3291359b0ca899f3b7b7c425
multilang <multilang@fd7de271-3064-49d4-864b-07bcf3d33c98>
2018-09-07 03:28:13 +0000

    [What]#0000:測試斷行字串
    git-svn-id: http://172.16.4.11/test_svn_server@95 fd7de271-3064-49d4-864b-07bcf3d33c98

--- trunk/multilang/Language/lang-en-US.js
+++ trunk/multilang/Language/lang-en-US.js
@@ -1,30 +1,13 @@
 _AS_STRINGS = {};
 
 _AS_STRINGS.LOGIN = {};
-_AS_STRINGS.LOGIN.STAY_SIGNED_IN = 'Stay signed in';
-_AS_STRINGS.LOGIN.SIGNING_IN = 'Signing in';
-_AS_STRINGS.LOGIN.LOADING = 'Loading';
-_AS_STRINGS.LOGIN.AUTH_FAIL = 'The username or password is incorrect. Please try again.';
-_AS_STRINGS.LOGIN.ACT_LOCK = 'Account disabled.';
-_AS_STRINGS.LOGIN.ACT_EXPIRE = 'Account has expired.';
-_AS_STRINGS.LOGIN.IEVERSION_ALERT = 'For the best user experience, it is recommended that you use Firefox, Chrome or Internet Explorer 9 and later.';
-_AS_STRINGS.LOGIN.SIGN_IN = 'Sign in';
-_AS_STRINGS.LOGIN.ONLINE_SUPPORT_FORM = 'Online Support Form';
-_AS_STRINGS.LOGIN.DOWNLOAD_CENTER = 'Download Center';
-_AS_STRINGS.LOGIN.VISIT_WEBSITE = 'Visit Website';
-_AS_STRINGS.LOGIN.SYSTEM_ANNOUNCEMENT = 'System announcement';
-_AS_STRINGS.LOGIN.COPYRIGHT = 'Copyright© 2018 ASUSTOR Inc.';
-_AS_STRINGS.LOGIN.MISS_PHONE = 'Lost mobile phone';
-_AS_STRINGS.LOGIN.ADMIN_MISS_PHONE = 'Please use the reset button found on the rear of the NAS to reset all account settings to their default values.';
-_AS_STRINGS.LOGIN.USER_MISS_PHONE = 'Please contact the system administrator.';
-_AS_STRINGS.LOGIN.ERROR_VERIFICATION_LENGTH = 'Please enter the 6-digit verification code';
-_AS_STRINGS.LOGIN.ERROR_VERIFICATION_CODE = 'Password is incorrect. Please try again.';
-_AS_STRINGS.LOGIN.ERROR_EXPIRING_DISABLED = 'Unable to login due to the account expiring or being disabled.';
 _AS_STRINGS.LOGIN.SUNDAY = 'Sunday';
 _AS_STRINGS.LOGIN.MONDAY = 'Monday';
 _AS_STRINGS.LOGIN.TUESDAY = 'Tuesday';
 _AS_STRINGS.LOGIN.WEDNESDAY = 'Wednesday';
-_AS_STRINGS.LOGIN.THURSDAY = 'Thursday';
+_AS_STRINGS.LOGIN.THURSDAY = 'Thur
+
+sday';
 _AS_STRINGS.LOGIN.FRIDAY = 'Friday';
 _AS_STRINGS.LOGIN.SATURDAY = 'Saturday';
 
