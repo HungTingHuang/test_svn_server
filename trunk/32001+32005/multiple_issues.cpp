4285cd1d6a8053d106626a44cf8fa6c3d987d2b6
choosinli <choosinli@fd7de271-3064-49d4-864b-07bcf3d33c98>
2019-11-15 04:29:00 +0000

    [What] #32005: Guide/demo of applying changes to branch and reverting changes - the second issue (may test conflict?) [Why] Changes for merging to branch which depends on 32001 [How] (omitted) [Who] (omitted)
    git-svn-id: http://172.16.4.11/test_svn_server@122 fd7de271-3064-49d4-864b-07bcf3d33c98

--- trunk/32001+32005/multiple_issues.cpp
+++ trunk/32001+32005/multiple_issues.cpp
@@ -3,8 +3,14 @@ static void functionA()
 	// do something for issue 32001
 }
 
+static void functionB()
+{
+	// do something for issue 32005
+}
+
 int main()
 {
 	functionA();
+	functionB();
 	return 0;
 }
